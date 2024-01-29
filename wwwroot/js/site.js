bd1b34d0dd94f153a695e81919e1ab179a855b24
kuaidy <732634939@qq.com>
2024-01-30 00:21:36 +0800

    new file:   Content/TableTools/Ascii.md

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,18 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$(function(){
-    testEditormdView = editormd.markdownToHTML("content_view", {
-        htmlDecode      : "style,script,iframe",  // you can filter tags decode
-        emoji           : true,
-        taskList        : true,
-        tex             : true,  // 默认不解析
-        flowChart       : true,  // 默认不解析
-        sequenceDiagram : true,  // 默认不解析
-        preview:true,
-        watch:true,
-        editor:false,
-        width:"100%"
-    });
-});
+//$(function(){
+//    testEditormdView = editormd.markdownToHTML("content_view", {
+//        htmlDecode      : "style,script,iframe",  // you can filter tags decode
+//        emoji           : true,
+//        taskList        : true,
+//        tex             : true,  // 默认不解析
+//        flowChart       : true,  // 默认不解析
+//        sequenceDiagram : true,  // 默认不解析
+//        preview:true,
+//        watch:true,
+//        editor:false,
+//        width:"100%"
+//    });
+//});
 
