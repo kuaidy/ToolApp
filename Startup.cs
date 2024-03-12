a233896c9e1f55222ac79eb7d8b864af841e8e4c
kuaidy <732634939@qq.com>
2024-03-12 23:14:19 +0800

    全站链接检测

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace ToolApp
             //×¢Èëblazor·þÎñ
             services.AddServerSideBlazor();
             services.AddRazorPages();
+            services.AddMasaBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
